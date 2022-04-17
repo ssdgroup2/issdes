5f03ad04f35ff346222cbfc97f6bcf3411a564d1
cryptopal85 <gurkanhuray@gm-macmini.local>
2022-04-17 11:08:40 +0000

    a fix for search fucntionality

--- app/app.py
+++ app/app.py
@@ -249,7 +249,7 @@ def processshare():
 
 
 ##### Download #####
-@app.route('/search-download-2', methods=['POST'])
+@app.route('/asfiledict', methods=['POST'])
 @login_required
 def getdownload():
 	# We need to confirm the state of radio buttons whether they are checked or not
